1e6caec29a8f8d244bcc48500f05ecd5ec9b0a73
Administrator <Administrator@MS-LSIQBZFPQYZK>
2020-10-13 18:06:21 +0800

    初始化提交

---
+++ src/07_01personController.js
@@ -0,0 +1,7 @@
+angular.module('myApp', []).controller('personCtrl', function($scope) {
+    $scope.firstName = "John",
+    $scope.lastName = "Doe",
+    $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName +"来自控制器js文件";
+    }
+});
